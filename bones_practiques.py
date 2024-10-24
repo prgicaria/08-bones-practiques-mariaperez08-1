9f6783f99c3f2792b7ce3345172a00a858aa9a88
mariaperez08 <mperez2@instituticaria.cat>
2024-10-24 09:08:25 +0200

    1r intent

---
+++ bones_practiques.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env phyton
+'''bones_practiques.py. Programa de Divisió
+Instiitut Icària
+Programació - 1r de Batxillerat - Curs 24/25
+Calcula la divisió entre un divident i divisor, que mostra per pantalla la divisió, el quocient i el residu. '''
+
+__author__ = "Maria Perez"
+__email__ = "mperez2@instituticaria.cat"
+__date__ = "2024/10/23"
+divident = int(input("introdueix el teu divient(nombre enter)"))
+divisor = int(input("introdueix el teu divisor(nombre enter)"))
+quocient= divident // divisor 
+residu = divident - quocient * divisor
+print(f"Divisió: {divident}/{divisor}")
+print(f"Quocient: {quocient}")
+print(f"Residu: {residu}")
